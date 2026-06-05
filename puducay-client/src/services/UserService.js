7232e04649f93861c19cbf154a200e9b7924c7ca
Eloisa Puducay <erpuducay@gmail.com>
2026-06-06 00:42:37 +0800

    production

--- puducay-client/src/services/UserService.js
+++ puducay-client/src/services/UserService.js
@@ -1,22 +1,15 @@
 import axios from 'axios';
-import constants from '../constants';
 
-// API Access to Front-end JSON data transformation or decoder
 const API = axios.create({
-    baseURL: `${constants.HOST}/users`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/users`,
 });
 
-// Fetch users
-export const fetchUsers = (user) => API.get('/', user);
+export const fetchUsers = () => API.get('/');
 
-// Create user
 export const createUser = (user) => API.post('/', user);
 
-// Update user
 export const updateUser = (id, user) => API.put(`/${id}`, user);
 
-// Delete user
 export const deleteUser = (id) => API.delete(`/${id}`);
 
-// Login user
 export const loginUser = (credentials) => API.post('/login', credentials);
