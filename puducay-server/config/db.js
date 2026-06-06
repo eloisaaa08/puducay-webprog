19cf3bff7b048717f7245bad597c1efb74e08599
Eloisa Puducay <erpuducay@gmail.com>
2026-06-06 16:50:35 +0800

    Fix MongoDB crash on Vercel

--- puducay-server/config/db.js
+++ puducay-server/config/db.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    // Connect MongoDB at default port 27017.
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1); // Exit process with failure
+  try {
+    if (!process.env.MONGO_URI) {
+      console.error("MONGO_URI is missing in Vercel Environment Variables");
+      return;
     }
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${error.message}`);
+  }
 };
 
 module.exports = connectDB;
