ba1bee8f3801f60870e43149585f90d5b33ccd2f
Eloisa Puducay <erpuducay@gmail.com>
2026-06-06 18:03:39 +0800

    fix api url

--- puducay-client/src/services/UserService.js
+++ puducay-client/src/services/UserService.js
@@ -1,24 +1,12 @@
-import axios from 'axios';
-import constants from '../constants';
+import axios from "axios";
+import constants from "../constants";
 
-// API Access to Front-end JSON data transformation or decoder
 const API = axios.create({
-    baseURL: `${constants.HOST}/users`,
+  baseURL: `${constants.HOST}/users`,
 });
 
-// Fetch users
-export const fetchUsers = (user) => API.get('/', user);
-
-// Create user
-export const createUser = (user) => API.post('/', user);
-
-// Update user
+export const fetchUsers = () => API.get("/");
+export const createUser = (user) => API.post("/", user);
 export const updateUser = (id, user) => API.put(`/${id}`, user);
-
-// Delete user
 export const deleteUser = (id) => API.delete(`/${id}`);
-
-// Login user
-export const loginUser = (credentials) => API.post('/login', credentials);
-
-console.log("HOST:", constants.HOST);
+export const loginUser = (credentials) => API.post("/login", credentials);
